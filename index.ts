2993561d35443c724ddc516af50ff44ff802bef5
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-08-12 14:44:25 +0530

    Learned how to work with functions, arrow functions, and typed objects in TypeScript

--- index.ts
+++ index.ts
@@ -73,4 +73,122 @@ let user3: User ={ //set types for alll keys
     city :"ampara"
 
 }
+//learned how to  working with  functions  using ts
 
+//want to parametr type  and retun type
+function function1(a:number , b:number):number{
+    return  a+b 
+
+
+
+}
+
+// If nothing is returned, the function should return void.
+
+function function2(a:number , b:number):void{
+    console.log(a+b)
+
+
+
+}
+
+
+function function3(a: number[]):void{
+
+    
+
+        console.log("my arrey ",a)
+
+    
+
+}
+
+let array:number[] = [1,2,3,4,5];
+
+function3(array);
+
+
+//arow function same as
+
+
+
+let function4= (a:number , b:number):void=>{
+
+    console.log(a+b);
+
+}
+
+
+
+let function5=(a:number, b:number ):number=>{
+
+
+    return a+b
+
+
+
+}
+
+
+// working with fnction with object 
+
+function function6(userObj:{name:string,age:number}):void{
+
+console.log( "myObject",userObj);
+
+}
+
+
+
+
+
+
+let userObj :{name:string ,age:number }={
+
+    name:"dilantha",
+    age :30
+
+}
+
+function6(userObj);
+
+
+
+// working with function with object custom type 
+
+
+ type newUser={
+    name: string,
+    age : 23
+ }
+
+
+
+
+function function7(user:newUser):void{
+
+    console.log("mynew Users",user)
+
+}
+
+
+
+let userObj2 :newUser={
+
+    name : "dilantha",
+    age:23
+
+}
+
+
+let userObj3 :newUser={
+
+    name : "dilantha nayanajith npx tsc",
+    age:23
+
+}
+
+function7(userObj2);
+
+
+function7(userObj3);
