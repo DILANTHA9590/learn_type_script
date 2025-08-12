264901c80fe4958a14986f3136baa93cf12c8b32
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-08-12 14:44:59 +0530

    🌟🌟

--- index.js
+++ index.js
@@ -9,52 +9,77 @@ let city = "ampara";
 let mobilenumber = "3333";
 let undifineOne = undefined;
 let mynull = null;
-let numberArrey = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-let stringArrey = ["a", "b", "c", "d", "e"];
-for (const val of stringArrey) {
-    console.log(val);
-}
-let obj = {
-    name: "dilantha",
-    age: 20,
-};
-console.log(obj);
-console.log(number1 + number2);
-let i = 10;
-for (i = 0; i < 10; i++) {
-    console.log(i);
-}
-for (i = 0; i < 10; i++) {
-    console.log(i);
-}
-let number3 = 10;
-let number4 = 13;
-if (number3 < number4) {
-    console.log(number3, "number 3 lower than number 4 ");
-}
-else {
-    console.log("number 4 lower than number 3");
-}
-let userData = [{
-        name: "dilantha",
-        age: "50"
-    },
-    {
-        name: "dilantha",
-        age: "50"
-    },
-    {
-        name: "dilantha",
-        age: "50"
-    },
-    {
-        name: "dilantha",
-        age: "50"
-    },
+//create string arrey
+let Number1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let string1 = ["a", "b", "c", "e", "f"];
+//create mixed aarrey  with union type
+let mixedArrey1 = ["dilantha", 2];
+let mixedArrey2 = ["dilantha", 2, false, true];
+//create t2d arrey
+const arrey = [
+    [1, 2, 3], [1, 2, 3]
 ];
-const data = userData.map((user) => {
-    return user.name;
-});
-console.log(data);
-console.log("samanatha");
+//fixed lenght  arrey 
+let fixedArrey1 = ["car", "bike"];
+let fixedArrey2 = ["car", 23];
+let fixedArrey3 = ["car", 32, 32];
+//Objects
+let user1 = {
+    name: "dilantha",
+    age: 34,
+    city: "ampara"
+};
+let user2 = {
+    name: "dilantha",
+    age: 34,
+    city: "ampara"
+};
+let user3 = {
+    name: "dilantha",
+    age: 34,
+    city: "ampara"
+};
+//learned how to  working with  functions  using ts
+//want to parametr type  and retun type
+function function1(a, b) {
+    return a + b;
+}
+// If nothing is returned, the function should return void.
+function function2(a, b) {
+    console.log(a + b);
+}
+function function3(a) {
+    console.log("my arrey ", a);
+}
+let array = [1, 2, 3, 4, 5];
+function3(array);
+//arow function same as
+let function4 = (a, b) => {
+    console.log(a + b);
+};
+let function5 = (a, b) => {
+    return a + b;
+};
+// working with fnction with object 
+function function6(userObj) {
+    console.log("myObject", userObj);
+}
+let userObj = {
+    name: "dilantha",
+    age: 30
+};
+function6(userObj);
+function function7(user) {
+    console.log("mynew Users", user);
+}
+let userObj2 = {
+    name: "dilantha",
+    age: 23
+};
+let userObj3 = {
+    name: "dilantha",
+    age: 23
+};
+function7(userObj2);
+function7(userObj3);
 //# sourceMappingURL=index.js.map
